d19fe0a5856da34a43df15094ff2f037a9404b0b
stiefn <pstiefn@gmail.com>
2021-06-17 12:04:35 +0200

    fixed dependencies

--- src/Editor.php
+++ src/Editor.php
@@ -2,7 +2,7 @@
 
 namespace Omines\DataTablesBundle;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMException;
